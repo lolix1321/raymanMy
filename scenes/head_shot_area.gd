ef5e3d3b39a95578b682e3251d69f23d6aff2795
lolix1321 <lolix.1321@gmail.com>
2026-02-01 13:54:00 +0100

    MEGA PORZADEK
    dsdsd

--- scenes/head_shot_area.gd
+++
@@ -1,2 +0,0 @@
-extends Area2D
-@export var blood_color: Color = Color.GREEN
--- scenes/sciezka_latania.gd
+++
@@ -1,6 +0,0 @@
-extends PathFollow2D
-
-@export var speed = 0.1 # Jak szybko lata
-
-func _process(delta):
-	progress_ratio += delta * speed
